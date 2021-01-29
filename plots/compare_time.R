85d8f539a245e601c2dd984f70baa0347a26c4f7
Nikita Zvonarev <nikitazvonarev@gmail.com>
2021-01-29 06:13:31 +0300

    Fix captions

--- plots/compare_time.R
+++ plots/compare_time.R
@@ -11,41 +11,41 @@ compare_time <- function(series, v_init, it, objective, subit = 16,
     print(step_search)
 
     weights_chol <- chol(weights)
-    
+
     if (step_search == "vp" && N >= vp_drop) {
         return(NA)
     }
-    
+
     inv_weights_chol <- NULL
-    
+
     if (step_search == "vp") {
         inv_weights_chol <- chol(solve(weights))
     }
-    
+
     signal <- NULL
     mats <<- NULL
     vspace_pack <<- NULL
-    
+
     v <- v_init
 
     fun_to_eval <- function() {
-        signal <- project_onto(series, v_init, weights_chol, inv_weights_chol, 
+        signal <- project_onto(series, v_init, weights_chol, inv_weights_chol,
                                compensated = compensated)
-        
+
         if (step_search == "vp") {
-            step <- find_step_vp(signal, series, v, project_onto = project_onto, 
+            step <- find_step_vp(signal, series, v, project_onto = project_onto,
                                  weights_chol = weights_chol,
                                  inv_weights_chol = inv_weights_chol)
         } else {
             step <- find_step(signal, series, v, vspace_pack, weights_chol)
-        }        
+        }
     }
-    
+
     times <- sapply(1:cycles, function(x) {
         print(x)
         system.time(fun_to_eval())["elapsed"]
     })
-    
+
     mean(times, trim = .2)
 }
 
@@ -54,9 +54,9 @@ eval_for_one_N <- function(N, weights, vp_drop = Inf) {
     signal <- seq(-1, 1, length.out = N)^2
     norm_const <- sqrt(sum(signal^2))
     signal <- signal / norm_const
-    
+
     net <- seq(from = -1, to = 1, length.out = N)
-    
+
     pre_noise <- abs(net) / sqrt(sum((abs(net))^2))
     polys <- legendre.polynomials(5)
     tang_space_basis <- cbind(as.function(polys[[1]])(net) / sqrt(N),
@@ -68,18 +68,18 @@ eval_for_one_N <- function(N, weights, vp_drop = Inf) {
 
     # weights <- Diagonal(N)
     # weights <- band_mat_from_diags(inv_ac_diags(N, c(0.9)))
-    
+
     weights_chol <- chol(weights)
-    
+
     noise <<- pre_noise -
-        weighted_project_onto_vspace_qr(pre_noise, 
+        weighted_project_onto_vspace_qr(pre_noise,
                                         weighted_project_rotate_basis(tang_space_basis, weights_chol), tang_space_basis, weights_chol)
-    
+
     series <- signal + noise
-    
+
     v <- c(1, -3, 3, -1) + c(1, 1, 1, 1) * 1e-6
-    
-    
+
+
     c(compare_time(series, v, it, signal, step_search = "mgn", weights = weights,
                    compensated = FALSE),
       compare_time(series, v, it, signal, step_search = "mgn", weights = weights),
@@ -88,7 +88,7 @@ eval_for_one_N <- function(N, weights, vp_drop = Inf) {
                                 vp_drop = vp_drop),
                   compare_time(series, v, it, signal, step_search = "vp", weights = weights,
                                vp_drop = vp_drop))
-    
+
 }
 
 colors <- c("black", "red", "green3", "blue")
@@ -103,11 +103,11 @@ white_noise_data <- sapply(net,
                            })
 
 
-matplot(net, t(white_noise_data), log = "xy", type = "l", 
+matplot(net, t(white_noise_data), log = "xy", type = "l",
         xlab = "N", ylab = "Time, sec", col = colors,
         lty = c(1,2,1,2))
 
-legend("topleft", c("MGN", "S-MGN", "VPGN", "S-VPGN"),
+legend("topleft", c("MGN", "MGN-H", "VPGN", "S-VPGN-H"),
        lty = c(1,2,1,2), bg = rgb(1, 1, 1, 0.85), col = colors)
 
 white_noise_data_moved <- t(white_noise_data)
@@ -116,11 +116,11 @@ white_noise_data_moved <- sapply(1:4, function(i) white_noise_data_moved[, i]/wh
 pdf("time_diagonal.pdf", width = 2.1, height = 2.1, pointsize = 4)
 par(mar = c(4.6,3.9,1.2,1.2))
 
-matplot(net, white_noise_data_moved, log = "xy", type = "l", 
+matplot(net, white_noise_data_moved, log = "xy", type = "l",
         xlab = "N", ylab = "Times slower", col = colors,
         lty = c(1,2,1,2))
 
-legend("topleft", c("MGN", "S-MGN", "VPGN", "S-VPGN"),
+legend("topleft", c("MGN", "MGN-H", "VPGN", "S-VPGN-H"),
        lty = c(1,2,1,2), bg = rgb(1, 1, 1, 0.85), col = colors)
 
 dev.off()
@@ -138,11 +138,11 @@ red_noise_data <- sapply(net,
 
 par(mar = c(4.6,3.9,1.2,1.2))
 
-matplot(net, t(red_noise_data), log = "xy", type = "l", 
+matplot(net, t(red_noise_data), log = "xy", type = "l",
         xlab = "N", ylab = "Time, sec", col = colors,
         lty = c(1,2,1,2))
 
-legend("topright", c("MGN", "S-MGN", "VPGN", "S-VPGN"),
+legend("topright", c("MGN", "MGN-H", "VPGN", "S-VPGN-H"),
        lty = c(1,2,1,2), bg = rgb(1, 1, 1, 0.85), col = colors)
 
 red_noise_data_moved <- t(red_noise_data)
@@ -151,10 +151,10 @@ red_noise_data_moved <- sapply(1:4, function(i) red_noise_data_moved[, i]/red_no
 pdf("time_3diagonal.pdf", width = 2.1, height = 2.1, pointsize = 4)
 par(mar = c(4.6,3.9,1.2,1.2))
 
-matplot(net, red_noise_data_moved, log = "xy", type = "l", 
+matplot(net, red_noise_data_moved, log = "xy", type = "l",
         xlab = "N", ylab = "Times slower", col = colors,
         lty = c(1,2,1,2))
 
-legend("topright", c("MGN", "S-MGN", "VPGN", "S-VPGN"),
+legend("topright", c("MGN", "MGN-H", "VPGN", "S-VPGN-H"),
        lty = c(1,2,1,2), bg = rgb(1, 1, 1, 0.85), col = colors)
 dev.off()
